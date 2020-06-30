5f37772b8bf31596191cc7aff7065b0bb510af91
Denise <denise.ndalamba@gmail.com>
2020-06-30 13:10:03 -0300

    protected routes middleware

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const router= express.Router()
+const { ensureAuth, ensureGuest } = require('../middleware/auth')
 
 // Login || Landing page
-router.get('/', (req, res) => {
+router.get('/', ensureGuest, (req, res) => {
     res.render('login', { layout: 'login'})
 })
 
 // Dashboard
 
-router.get('/dashboard', (req, res) => {
+router.get('/dashboard', ensureAuth, (req, res) => {
     res.render('dashboard')
 })
 
