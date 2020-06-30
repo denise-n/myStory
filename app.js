9232fc09578819ba6fed1b3152f3d1f6b0adc561
Denise <denise.ndalamba@gmail.com>
2020-06-30 10:48:13 -0300

    create passport config & sessions

--- app.js
+++ app.js
@@ -3,10 +3,15 @@ const express = require('express')
 const dotenv = require('dotenv')
 const exphbs = require('express-handlebars')
 const morgan = require('morgan')
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
 
 connectDB()
 
@@ -21,6 +26,16 @@ if(process.env.NODE_ENV === 'development') {
 app.engine('.hbs', exphbs({extname: '.hbs', defaultLayout: 'main.hbs'}))
 app.set('view engine', '.hbs')
 
+// Sessions
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: false,
+}))
+
+// Passport middleware
+app.use(passport.initialize())
+app.use(passport.session())
 // Static Folder
 app.use(express.static(path.join(__dirname, 'public')))
 
